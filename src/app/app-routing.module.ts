845deeaddd7fad8f05a92ed6d239b7f1593c2595
andres.castro <andres.castro@pragma.com.co>
2019-09-24 15:19:05 -0500

    add home and car components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CarComponent } from './car/car.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'car', component: CarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/car/car.model.ts
@@ -0,0 +1,8 @@
+export class Car {
+    constructor(
+        model: string,
+        year: string,
+        color: string,
+        horsePower: string
+    ){}
+}
