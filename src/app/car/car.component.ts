05c60b7b8cf31beccd959466cd3b3e01c3721d44
andres.castro <andres.castro@pragma.com.co>
2019-09-25 17:41:04 -0500

    add submit a car

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -9,13 +9,22 @@ import { Car } from "./car.model";
 export class CarComponent implements OnInit {
 
   car: Car;
+  carArray: Array<Car> = new Array;
 
   constructor() {
     this.car = new Car("","","","");
   }
 
+  addCar() {
+    this.carArray.push(this.car);
+  }
+
   ngOnInit() {
     
   }
-
+  
+  onSubmit() {
+    // this.addCar();
+    console.log(this.car);
+  }
 }
