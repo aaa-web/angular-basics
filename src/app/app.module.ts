ed8634b008153cbb676182820bbc303a2f6ae618
andres.castro <andres.castro@pragma.com.co>
2019-10-02 13:13:43 -0500

    adding http service to get all cars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from "@angular/material/card";
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { MatCardModule } from "@angular/material/card";
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
-import {  } from "module";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+  urlAllCars = 'https://my-json-server.typicode.com/aaa-web/demo-db';
+  
+  getCars() {
+    return this
+    .http
+    .get(`${this.urlAllCars}`);
+  }
 }
